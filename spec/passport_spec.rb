ec13eedb40a43fa14d2d54e5851f4e8443142d7c
André Luiz Leoni <andreluizleoni@gmail.com>
2017-08-26 08:17:51 -0300

    Update passport_spec.rb

--- spec/passport_spec.rb
+++ spec/passport_spec.rb
@@ -3,8 +3,12 @@ require 'date'
 require_relative '../app/passport.rb'
 
 RSpec.describe Passport  do
-  context 'SENDO usuário logado, QUANDO vou criar uma passagem' do   
-    it 'com dados inválidos, DEVO receber o retorno de que dados não conferem' do
+  context 'SENDO usuário logado, 
+           DEVO poder criar uma passagem 
+           PARA que o sistema faça a compra com a companhia aérea' do   
+    it 'DADO QUE eu tenha um usuário cadastrado no sistema com permissões de administrador 
+        QUANDO colocar as informações de usuário e senha 
+        ENTÃO entrará na tela de permissões administrativas.' do
       expect(Passport.create(DateTime.parse('28/08/2017'), DateTime.parse('26/08/2017'))).to eql("Data de chegada nao pode ser menor que data de saída")
     end
     
@@ -12,4 +16,10 @@ RSpec.describe Passport  do
       expect(Passport.create(DateTime.parse('28/08/2017'), DateTime.parse('28/09/2017'))).to eql("Passagem criada com sucesso")
     end
   end
+end
+
+RSpec.describe Payment do
+end
+
+RSpec.describe Advertisement do
 end
